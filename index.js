ce64869efc1aa79eae8a7890dd673a4b89553fdd
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-02 21:29:27 +0600

    add db

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import 'dotenv/config'
-import connectDB from './src/db/connectDB.js'
+import "dotenv/config";
+import connectDB from "./src/db/connectDB.js";
+import { app } from "./src/app.js";
 
-
-
-
-connectDB();
-
-// console.log(process.env.DB);
-
+connectDB()
+.then(() => {
+    // error losten
+    app.on( "error" ,(error) => {
+        console.log("Error", error);
+        throw error;
+    })
+    // 
+    app.listen(process.env.PORT || 8080, () =>
+      console.log(`Server is running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log("Mongo DB connection failed:", err));
