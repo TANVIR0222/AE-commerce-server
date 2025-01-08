d00b025169b0eb72010095d43e9595388bb180dc
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-08 23:44:24 +0600

    Add new Address

---
+++ src/controller/address.controller.js
@@ -0,0 +1,44 @@
+import AddressModel from "../model/address.model.js";
+import { UserModel } from "../model/user.model.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+
+// Add a new address
+const addAddress = asyncHandler(async (req,res) => {
+
+    const {id} = req.params;
+    const {addressline,city,state,pincode,country,mobile} = req.body;
+
+    // check if user exists
+    if(
+        [addressline,city,state,pincode,country,mobile].some((field) => field.trime === "")
+    ){
+        throw new ApiError(400, "Please fill in all fields");
+    }
+
+
+    // save address
+    const userAddress = await AddressModel({
+        userId : id,
+        addressline,
+        city,
+        state,
+        pincode,
+        country,
+        mobile,
+    });
+
+    // save address to database
+    const savedAddress = await userAddress.save();
+
+
+    // update user with new address
+    const user = await UserModel.updateOne({_id : id} , {$push : {address_details : savedAddress._id}} , {new : true});
+    res.status(201).json(new ApiResponse(201 , user , "Address added successfully"));
+
+    
+})
+
+export { addAddress }
